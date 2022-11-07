a273fc3f6ea805531ed5aa7cfe602085c07d1d73
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-11-07 15:11:32 -0700

    created decision deciding file

---
+++ decisionType.py
@@ -0,0 +1,7 @@
+
+
+def decisionType(): # return response that user wants to get decision on
+    answer = input("Input:\n1 to load random anime\n2 to load random show\n3 to make and create random decision\n- Anything else to logout\n")
+    return answer
+    
+decisionType()
